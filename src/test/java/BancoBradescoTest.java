95b2884a976170b81f1b244a121797fe96174068
PedroHPMarques <pedro.marques@estudante.ufjf.br>
2023-10-02 00:52:03 -0300

    altera nomes do test Bradesco

--- src/test/java/BancoBradescoTest.java
+++ src/test/java/BancoBradescoTest.java
@@ -4,21 +4,21 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class BancoBradescoTest {
     @Test
-    void deveRetornarBancoItauViaPix() {
+    void deveRetornarBancoBradescoViaPix() {
         Pagamento tipoPagamento = new Pix();
         BancoBradesco banco = new BancoBradesco(tipoPagamento);
         assertEquals("Banco Bradesco pagamento no pix", banco.transacao());
     }
 
     @Test
-    void deveRetornarBancoItauViaCredito() {
+    void deveRetornarBancoBradescoViaCredito() {
         Pagamento tipoPagamento = new Credito();
         BancoBradesco banco = new BancoBradesco(tipoPagamento);
         assertEquals("Banco Bradesco pagamento no crédito", banco.transacao());
     }
 
     @Test
-    void deveRetornarBancoItauViaDebito() {
+    void deveRetornarBancoBradescoViaDebito() {
         Pagamento tipoPagamento = new Debito();
         BancoBradesco banco = new BancoBradesco(tipoPagamento);
         assertEquals("Banco Bradesco pagamento no débito", banco.transacao());
